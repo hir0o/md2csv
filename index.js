3abcb5b7681726a7a02342df52194531a5c808cc
mkmk4423 <mk.9.q.kyu@gmail.com>
2021-02-19 20:43:04 +0900

    一旦完成

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
-var fs = require("fs");
-var formatCSV = "";
+const fs = require("fs");
+const { listFiles } = require("./lib/listFiles");
+const { exportCSV } = require("./lib/exportCSV");
 
-let text = fs.readFileSync("test.md", 'utf-8');
+const csvFileName = "course.csv";
+const filePaths = listFiles("Pythonを始めよう");
 
-//*   ["タイトル", "本文", "クイズ内容", "クイズ解説", "選択肢1", "正解かどうか", "選択肢2", "正解かどうか", "選択肢3", "正解かどうか", "選択肢4", "正解かどうか"]
+const lessons = [];
 
-// レッスンごとに分割
-const sliceChapter = text.split("---").map(t => {
-  return [t.replace(/^\s+|\s+$/g, '')]
+filePaths.forEach((filePath) => {
+  let file = fs.readFileSync(filePath, "utf-8");
+
+  // レッスンごとに分割
+  const sliceChapter = [];
+  file.split(/\n---/).forEach((lesson, i) => {
+    if (i <= 0) {
+      return;
+    }
+    sliceChapter.push(
+      lesson
+        .replace(/<!-- ?\r?\n?＠TODO\r?\n([\s\S]+?)\r?\n?-->/g, "") // @TODOを削除
+        .replace(/^\s+|\s+$/g, "")
+    );
+  });
+
+  sliceChapter.forEach((chapter) => {
+    // タイトル
+    const title = chapter.match(/^## .+/g)[0].replace("## ", "");
+    // 概要
+    const desc = chapter
+      .match(/#{3} ?概要\r?\n([\s\S]+?)\r?\n#{3}/g)[0]
+      .replace(/#{3} ?(.+)?/g, "")
+      .replace(/^\s+|\s+$/g, "");
+    // 経験値
+    const exp = chapter
+      .match(/#{3} ?経験値\r?\n([\s\S]+?)\r?\n#{3}/g)[0]
+      .replace(/#{3} ?(.+)?/g, "")
+      .replace(/^\s+|\s+$/g, "");
+    const body = chapter
+      .match(/#{3} ?本文\r?\n([\s\S]+?)\r?\n#{3} ?問題/g)[0]
+      .replace(/#{3} ?(.+)?/g, "")
+      .replace(/^\s+|\s+$/g, "");
+    // 問題文
+    const quizBody = chapter
+      .match(/#{3} 問題\r?\n([\s\S]+?)\r?\n- \[( |x)?\]/)[1]
+      .replace(/^\s+|\s+$/g, "");
+    // 解説
+    const commentary = chapter
+      .replace(/(#{2} .+)/, "")
+      .split(/(#{3} 解説)/)[2]
+      .replace(/^\s+|\s+$/g, "");
+    // 選択肢を配列で取得
+    const quizChoices = [];
+    chapter.match(/(- \[( |x)\] .+\n)/g).forEach((choice) => {
+      const choiceSplit = choice.replace("\n", "").split(/(- \[ ?x?\])/);
+      isCorrect = choiceSplit[1].includes("x");
+      quizChoiceText = choiceSplit[2];
+      quizChoices.push(quizChoiceText, isCorrect);
+    });
+    lessons.push([
+      title,
+      desc,
+      exp,
+      body,
+      quizBody,
+      commentary,
+      ...quizChoices,
+    ]);
+  });
 });
 
-const sliceQuiz = sliceChapter.map(chapter =>  {
-  // タイトルを取得
-  const title = chapter[0].match(/#{2} .+/)[0].replace("## ", "")
-  // bodyを取得
-  const body = chapter[0].replace(/(#{2} .+)/, "").split(/(#{3} 問題)/)[0].replace(/^\s+|\s+$/g, '');
-  // 問題文を取得
-  const quizBody = chapter[0].replace(/(#{2} .+)/, "").split(/(#{3} 問題)/)[2].split('- [')[0].replace(/^\s+|\s+$/g, '');
-  // 解説を取得
-  const commentary = chapter[0].replace(/(#{2} .+)/, "").split(/(#{3} 解説)/)[2].replace(/^\s+|\s+$/g, '');
-  // 選択肢を配列で取得
-  const quizChoices = []
-  chapter[0].match(/(- \[( |x)\] .+\n)/g).forEach(choice => {
-    const choiceSplit = choice.replace('\n', '').split(/(- \[ ?x?\])/) // !! 配列の先頭に空文字が入っちゃう
-    isCorrect = choiceSplit[1].includes('x')
-    quizChoiceText = choiceSplit[2]
-    quizChoices.push(quizChoiceText, isCorrect)
-  })
-
-  return [title, body, quizBody, commentary, ...quizChoices]
-})
-
-// console.log(sliceQuiz)
-
-exportCSV(sliceQuiz);
-
-// 配列をcsvで保存する関数
-function exportCSV(content) {
-  for (var i = 0; i < content.length; i++) {
-    var value = content[i];
-
-    for (var j = 0; j < value.length; j++) {
-      var innerValue = value[j] === null ? "" : value[j].toString();
-      var result = innerValue.replace(/"/g, '""');
-      if (result.search(/("|,|\n)/g) >= 0) result = '"' + result + '"';
-      if (j > 0) formatCSV += ",";
-      formatCSV += result;
-    }
-    formatCSV += "\n";
-  }
-  fs.writeFile("cource.csv", formatCSV, "utf8", function (err) {
-    if (err) {
-      console.log("保存できませんでした");
-    } else {
-      console.log("保存できました");
-    }
-  });
-}
+exportCSV(lessons, csvFileName);
