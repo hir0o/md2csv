3616259dcfe9cd2c8629fa5537338adf49239379
hiroyuki <mk.9.q.kyu@gmail.com>
2021-06-24 21:58:58 +0900

    少し整理

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const { listFiles } = require("./lib/listFiles");
 const { exportCSV } = require("./lib/exportCSV");
 const { shuffle } = require("./lib/shuffle");
+const { extractCharacters } = require("./lib/extractCharacters");
 
 const csvFileName = "course.csv";
 const filePaths = listFiles(process.argv[2]);
@@ -31,23 +32,8 @@ filePaths.forEach((filePath) => {
   if (textType === "q" && filePath.includes("まとめテスト")) {
     sliceChapter.forEach((chapter) => {
       try {
-        // 問題文
-        const quizBody = chapter
-          .match(/#{3} 問題\r?\n([\s\S]+?)\r?\n- \[( |x)?\]/)[1]
-          .replace(/^\s+|\s+$/g, "");
-        // 解説
-        const commentary = chapter
-          .replace(/(#{2} .+)/, "")
-          .split(/(#{3} 解説)/)[2]
-          .replace(/^\s+|\s+$/g, "");
-        // 選択肢を配列で取得
-        const quizChoices = [];
-        chapter.match(/(- \[( |x)\] .+\n)/g).forEach((choice) => {
-          const choiceSplit = choice.replace("\n", "").split(/(- \[ ?x?\])/);
-          const isCorrect = choiceSplit[1].includes("x");
-          const quizChoiceText = choiceSplit[2];
-          quizChoices.push(quizChoiceText, isCorrect);
-        });
+        const { quizBody, commentary, quizChoices } = extractCharacters(strs);
+
         csv.push([quizBody, commentary, ...quizChoices]);
       } catch {
         isError = true;
@@ -60,40 +46,9 @@ filePaths.forEach((filePath) => {
     if (!filePath.includes("まとめテスト")) {
       sliceChapter.forEach((chapter) => {
         try {
-          // タイトル
-          const title = chapter.match(/^## .+/g)[0].replace("## ", "");
-          // 概要
-          const desc = chapter
-            .match(/#{3} ?概要\r?\n([\s\S]+?)\r?\n#{3}/g)[0]
-            .replace(/#{3} ?(.+)?/g, "")
-            .replace(/^\s+|\s+$/g, "");
+          const { title, desc, exp, body, quizBody, commentary, quizChoices } =
+            extractCharacters(chapter);
 
-          // 経験値
-          const exp = chapter
-            .match(/#{3} ?経験値\r?\n([\s\S]+?)\r?\n#{3}/g)[0]
-            .replace(/#{3} ?(.+)?/g, "")
-            .replace(/^\s+|\s+$/g, "");
-          const body = chapter
-            .match(/#{3} ?本文\r?\n([\s\S]+?)\r?\n#{3} ?問題/g)[0]
-            .replace(/#{3} ?(.+)?/g, "")
-            .replace(/^\s+|\s+$/g, "");
-          // 問題文
-          const quizBody = chapter
-            .match(/#{3} 問題\r?\n([\s\S]+?)\r?\n- \[( |x)?\]/)[1]
-            .replace(/^\s+|\s+$/g, "");
-          // 解説
-          const commentary = chapter
-            .replace(/(#{2} .+)/, "")
-            .split(/(#{3} 解説)/)[2]
-            .replace(/^\s+|\s+$/g, "");
-          // 選択肢を配列で取得
-          const quizChoices = [];
-          chapter.match(/(- \[( |x)\] .+\n)/g).forEach((choice) => {
-            const choiceSplit = choice.replace("\n", "").split(/(- \[ ?x?\])/);
-            const isCorrect = choiceSplit[1].includes("x");
-            const quizChoiceText = choiceSplit[2];
-            quizChoices.push(quizChoiceText, isCorrect);
-          });
           if (textType === "s") {
             csv.push([quizBody, commentary, ...quizChoices]);
           } else {
@@ -135,8 +90,7 @@ if (!isError) {
     ]);
     exportCSV(arr, csvFileName);
   } else {
-    const arr = shuffle(csv);
-    arr.unshift([
+    csv.unshift([
       "タイトル",
       "概要",
       "経験値",
@@ -152,6 +106,6 @@ if (!isError) {
       "選択肢4",
       "正解かどうか",
     ]);
-    exportCSV(arr, csvFileName);
+    exportCSV(csv, csvFileName);
   }
 }
